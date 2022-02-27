ee65c164b2e8aeaa72c3feb5f0567e99d24ac20e
Tama <15ej025@ms.dendai.ac.jp>
2022-02-27 19:03:54 +0900

    Update main.js
    検索する要素を 'video' のみに変更
    -> 実質ニコ生対応
    -> ニコ生以外も対応？

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-javascript:(function(n){n.style.transform=(n.style.transform.toLowerCase()==='scalex(-1)')?'':'scaleX(-1)';})(document.querySelector('#MainVideoPlayer').querySelector('video'));
+javascript:(
+    function(n){
+        if (!n) {
+            return;
+        }
+        n.style.transform=(n.style.transform.toLowerCase()==='scalex(-1)')?'':'scaleX(-1)';
+    }
+)
+(
+    function(){
+        const nico = document.querySelector('video');
+        if (nico) {
+            return nico;
+        }
+        return null;
+    }()
+);
